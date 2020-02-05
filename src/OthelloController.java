7022e2f5c6ca7071f33667980396ee6031f0da72
skytomo <tomo1kawa.fullerene@gmail.com>
2020-02-05 14:55:59 +0900

    Add select in keyboard

--- src/OthelloController.java
+++ src/OthelloController.java
@@ -5,99 +5,114 @@ import java.awt.event.ComponentListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.regex.Pattern;
 
 public class OthelloController implements ActionListener, ComponentListener, MouseListener, MouseMotionListener {
-    protected OthelloViewer viewer;
+	protected OthelloViewer viewer;
 
-    protected Othello othello;
+	protected Othello othello;
 
-    public OthelloController() {
-        this.othello = new Othello();
-    }
+	public OthelloController() {
+		this.othello = new Othello();
+	}
 
-    public void setViewer(OthelloViewer viewer) {
-        this.viewer = viewer;
-        viewer.addActionListener(this);
-        viewer.addMouseListener(this);
-        viewer.boardDrawer.addMouseListener(this);
-        viewer.boardDrawer.addMouseMotionListener(this);
-    }
+	public void setViewer(OthelloViewer viewer) {
+		this.viewer = viewer;
+		viewer.addActionListener(this);
+		viewer.addMouseListener(this);
+		viewer.boardDrawer.addMouseListener(this);
+		viewer.boardDrawer.addMouseMotionListener(this);
+		viewer.sendButton.addMouseListener(this);
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
+	public void parseInput() {
+		String text = viewer.textField.getText();
+		if (Pattern.matches("\\d+", text)) {
+			int number = Integer.parseInt(text);
+			int index = number - 1;
+			if (index < othello.getCanPutPieceList().size()) {
+				othello.putPiece(othello.getCanPutPieceList().get(index));
+			}
+		}
+	}
 
-    }
+	@Override
+	public void actionPerformed(ActionEvent actionEvent) {
 
-    @Override
-    public void componentResized(ComponentEvent componentEvent) {
+	}
 
-    }
+	@Override
+	public void componentResized(ComponentEvent componentEvent) {
 
-    @Override
-    public void componentMoved(ComponentEvent componentEvent) {
+	}
 
-    }
+	@Override
+	public void componentMoved(ComponentEvent componentEvent) {
 
-    @Override
-    public void componentShown(ComponentEvent componentEvent) {
+	}
 
-    }
+	@Override
+	public void componentShown(ComponentEvent componentEvent) {
 
-    @Override
-    public void componentHidden(ComponentEvent componentEvent) {
+	}
 
-    }
+	@Override
+	public void componentHidden(ComponentEvent componentEvent) {
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (e.getSource() instanceof BoardDrawer) {
-            BoardDrawer p = (BoardDrawer) e.getSource();
-            Coordinate c = p.getCoordinate(e.getPoint());
-            othello.putPiece(c);
-            p.repaint();
-            if (othello.finish()) {
-                int black = othello.count(OthelloPiece.Color.BLACK);
-                int white = othello.count(OthelloPiece.Color.WHITE);
-                String message = othello.turn + "ターン目で，終了しました．" + black + "対" + white + "で";
-                if (black > white) {
-                    message += "黒の勝ちです！";
-                } else if (black < white) {
-                    message += "白の勝ちです！";
-                } else {
-                    message += "引き分けです！";
-                }
-                viewer.northLabel.setText(message);
-            } else {
-                int black = othello.count(OthelloPiece.Color.BLACK);
-                int white = othello.count(OthelloPiece.Color.WHITE);
-                String message = (othello.currentColor == OthelloPiece.Color.BLACK ? "黒" : "白") + "の番です．";
-                message += "現在，" + othello.turn + "ターン目です．" + black + "対" + white + "です．";
-                viewer.northLabel.setText(message);
-            }
-        }
-    }
+	}
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-        // TODO 自動生成されたメソッド・スタブ
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getSource() instanceof BoardDrawer) {
+			BoardDrawer p = (BoardDrawer) e.getSource();
+			Coordinate c = p.getCoordinate(e.getPoint());
+			othello.putPiece(c);
+		} else if (e.getSource() == viewer.sendButton) {
+			parseInput();
+		}
+		viewer.repaint();
+		if (othello.finish()) {
+			int black = othello.count(OthelloPiece.Color.BLACK);
+			int white = othello.count(OthelloPiece.Color.WHITE);
+			String message = othello.turn + "ターン目で，終了しました．" + black + "対" + white + "で";
+			if (black > white) {
+				message += "黒の勝ちです！";
+			} else if (black < white) {
+				message += "白の勝ちです！";
+			} else {
+				message += "引き分けです！";
+			}
+			viewer.northLabel.setText(message);
+		} else {
+			int black = othello.count(OthelloPiece.Color.BLACK);
+			int white = othello.count(OthelloPiece.Color.WHITE);
+			String message = (othello.currentColor == OthelloPiece.Color.BLACK ? "黒" : "白") + "の番です．";
+			message += "現在，" + othello.turn + "ターン目です．" + black + "対" + white + "です．";
+			viewer.northLabel.setText(message);
+		}
+	}
 
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        // TODO 自動生成されたメソッド・スタブ
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
+	@Override
+	public void mousePressed(MouseEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-    }
 
-    @Override
-    public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-    }
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+	}
 
 	@Override
 	public void mouseDragged(MouseEvent mouseEvent) {
--- src/OthelloViewer.java
+++ src/OthelloViewer.java
@@ -1,21 +1,24 @@
+import java.awt.BorderLayout;
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
-import java.awt.BorderLayout;
-import java.awt.Font;
 
 public class OthelloViewer extends JFrame {
 
     BoardDrawer boardDrawer;
     JLabel northLabel;
+    JTextField textField;
+    JButton sendButton;
 
     public OthelloViewer(Othello othello) {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Othello");
-        setBounds(100, 100, 450, 300);
+        setBounds(100, 100, 700, 800);
         JPanel panel = new JPanel();
         panel.setBorder(new EmptyBorder(5, 5, 5, 5));
         panel.setLayout(new BorderLayout(0, 0));
@@ -24,15 +27,15 @@ public class OthelloViewer extends JFrame {
         JLabel southLabel = new JLabel("コマンドを入力して戦闘開始．23を入力すると(x = 2, y = 3)のところにコマを置く．");
         northLabel = new JLabel((othello.currentColor == OthelloPiece.Color.BLACK ? "黒" : "白") + "の番です．");
         northLabel.setFont( new Font("SanSerif" , Font.BOLD , 20));
-        JTextField textField = new JTextField();
+        textField = new JTextField();
         textField.setText("");
-        JButton button = new JButton("送信");
+        sendButton = new JButton("送信");
         //textField.setColumns(30);
         southPanel.setLayout(new BorderLayout());
         southPanel.add(northLabel, BorderLayout.NORTH);
         southPanel.add(southLabel, BorderLayout.SOUTH);
         southPanel.add(textField, BorderLayout.CENTER);
-        southPanel.add(button, BorderLayout.EAST);
+        southPanel.add(sendButton, BorderLayout.EAST);
         boardDrawer = new BoardDrawer(othello);
         panel.add(boardDrawer, BorderLayout.CENTER);
         panel.add(southPanel, BorderLayout.SOUTH);
